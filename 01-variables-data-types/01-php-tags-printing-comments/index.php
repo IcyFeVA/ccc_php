6340ac60adf9ee0312131a28ab5db309f6ee03f8
IcyFeVA <hi@icyfeva.com>
2024-12-03 08:04:01 -0800

    the basics explained

--- 01-variables-data-types/01-php-tags-printing-comments/index.php
+++ 01-variables-data-types/01-php-tags-printing-comments/index.php
@@ -1,5 +1,40 @@
+<?php
+    $teachersName = "Bob";
+    $cardHeading = "Welcome To The Course";
+    $paragraph = "<p>In this course, you will learn the fundamentals of the PHP language</p>";
+
+    $aString = "This is a string";      // String
+    $myFavoriteNumber = 56445628;       // Integer
+    $decimal = 3.4156456655;            // Floating Point Number
+    $isLoggedIn = false;                // Boolean
+    $middleName = null;                 // Empty variable
+    $object = [];                       // Object / Array / Function
+    $letsGo = new stdClass();           // Class    
+
+    // var_dump($decimal);              // console log for php
+
+    $birthYear = 1981;
+    $birthDay = 23;
+
+    $result = $birthDay / $birthYear;
+    var_dump($result);
+
+    $firstname = "Ting-yu";
+    $lastname = "Chan";
+    $fullName = $firstname . " " . $lastname;
+
+    var_dump($fullName);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
+<script>
+    const firstname = "Pascal"
+    const lastName = "Zirn"
+
+    const combined = firstname + " " + lastName
+</script>
 
 <head>
     <meta charset="UTF-8">
@@ -11,15 +46,15 @@
 <body class="bg-gray-100">
     <header class="bg-blue-500 text-white p-4">
         <div class="container mx-auto">
-            <h1 class="text-3xl font-semibold">Learn PHP From Scratch</h1>
+            <h1 class="text-3xl font-semibold"><?= "Hello $fullName" ?></h1>
         </div>
     </header>
+    
     <div class="container mx-auto p-4 mt-4">
         <div class="bg-white rounded-lg shadow-md p-6">
-            <h2 class="text-2xl font-semibold mb-4">Welcome To The Course</h2>
-            <p>In this course, you will learn the fundamentals of the PHP language</p>
+            <h2 class="text-2xl font-semibold mb-4"><?= $cardHeading ?></h2>
+            <?= $paragraph ?>
         </div>
     </div>
 </body>
-
 </html>
